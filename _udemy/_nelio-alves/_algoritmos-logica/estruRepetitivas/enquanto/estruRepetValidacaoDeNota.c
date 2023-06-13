c52e820e0ef5e2f8917b5312addd7fab99a574ba
Caio Renan <92604099+Caio-Renan@users.noreply.github.com>
2023-06-12 22:05:34 -0300

    Update
    - Remember to come back here later to learn how to do a better solution.

--- _udemy/_nelio-alves/_algoritmos-logica/estruRepetitivas/enquanto/estruRepetValidacaoDeNota.c
+++ _udemy/_nelio-alves/_algoritmos-logica/estruRepetitivas/enquanto/estruRepetValidacaoDeNota.c
@@ -1,14 +1,41 @@
 #include <stdio.h>
+#include <math.h>
+
+// Tried but can't make it works
+//void wrong_value() {
+//    while (n < 0) {
+//        printf("Valor inválido! Tente novamente: ");
+//        scanf("%lf", &n);
+//    }
+//}
+
 
 int main() {
-    double n1, n2, media;
+    double n, total, media;
+    int i;
 
-    printf("Digite a primeira nota: ");
-    scanf("%lf", %n1)
-    printf("Digite a segunda nota: ");
-    scanf("%lf", %n2)
-    media = n1 + n2 /
+    for (i = 1; i < 3; i++) {
+        total = 0;
+        printf("Digite a primeira nota: ");
+        scanf("%lf", &n);
+        while (n < 0 || n > 10) {
+            printf("Valor invalido! Tente novamente (primeira nota): ");
+            scanf("%lf", &n);
+        }
+        total = total + n;
+        printf("Digite a segunda nota: ");
+        scanf("%lf", &n);
+        printf("\n");
+        while (n < 0 || n > 10) {
+            printf("Valor invalido! Tente novamente (segunda nota): ");
+            scanf("%lf", &n);
+        }
+        total = total + n;
 
+        media = total / 2;
+
+        printf("MEDIA = %.2lf\n\n", media);
+    }
 
     return 0;
 }
